709e64ab0b2c265b60dde9bd9826a2beccf531fb
Neetesh NG <neeteshng15@gmail.com>
2023-10-31 18:52:34 +0530

    Display a Digital Watch that Shows Real Time. (Using react-moment)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import moment from "moment";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(moment().format("LTS"))
+    }, 1000);
+  }, [time]);
+
   return (
     <div className="App">
-
+      <h1>Current Time</h1>
+      {time}
     </div>
   );
 }
